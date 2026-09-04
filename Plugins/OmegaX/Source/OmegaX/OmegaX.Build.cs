4313a1c8f8e45c2e374c16de5a13d55ddcce3fba
fotografandrzejmikulski-bit <fotografandrzejmikulski@gmail.com>
2026-09-04 05:04:49 +0200

    feat: add Unreal Engine module build definition

---
+++ Plugins/OmegaX/Source/OmegaX/OmegaX.Build.cs
@@ -0,0 +1,16 @@
+using UnrealBuildTool;
+
+public class OmegaX : ModuleRules
+{
+    public OmegaX(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new[]
+            {
+                "Core"
+            }
+        );
+    }
+}
